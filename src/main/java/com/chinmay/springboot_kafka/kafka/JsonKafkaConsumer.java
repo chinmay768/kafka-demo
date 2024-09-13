36e69668f904aa308d8e2e6befc3e0658d255c72
chinmay768 <chinmaysahani07@gmail.com>
2024-09-14 00:05:14 +0530

    Feat: Json Deserializer Fix

--- src/main/java/com/chinmay/springboot_kafka/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/chinmay/springboot_kafka/kafka/JsonKafkaConsumer.java
@@ -1,5 +1,18 @@
 package com.chinmay.springboot_kafka.kafka;
 
-public class JsonKafkaConsumer {
 
+import com.chinmay.springboot_kafka.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "chinmay_json", groupId = "myGroup")
+    public void consume(User user){
+        LOGGER.info(String.format("Json message recieved -> %s", user.toString()));
+    }
 }
